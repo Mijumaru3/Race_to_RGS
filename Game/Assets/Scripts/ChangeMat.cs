0a55c1e7cc06036de2fc54604eec4bb7b9ea36ee
Andy <Mijumaru13@gmail.com>
2015-10-17 23:42:18 -0400

    Comments
    Commented scripts for easier understanding

--- Game/Assets/Scripts/ChangeMat.cs
+++ Game/Assets/Scripts/ChangeMat.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class ChangeMat : MonoBehaviour {
 
 	//public variables
-	public PhysicsMaterial2D[] mats;
+	public PhysicsMaterial2D[] mats; //array of materials of the block
 
 	//private variables
-	Collider2D col;
-	PhysicsMaterial2D current;
+	Collider2D col; //collider of the block
+	PhysicsMaterial2D current; //the current material of the block
 	
 	// Use this for initialization
 	void Start () {
-		col = GetComponent<Collider2D> ();
-		current = null;
+		col = GetComponent<Collider2D> (); //get reference to the collider of the block
+		current = null; //initialize current material to null - meaning no material in effect
 	}
 	
 	// Update is called once per frame
@@ -23,22 +23,15 @@ public class ChangeMat : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D obj)
 	{
+		//if the game object that collides with the block's collider has the tag "bullet"
 		if (obj.gameObject.tag == "bullet") {
-			int type = obj.gameObject.GetComponent<BulletScript>().Type();
-			current = mats[type];
-			col.sharedMaterial = current;
-			col.enabled = false;
-			col.enabled = true;
+			int type = obj.gameObject.GetComponent<BulletScript>().Type(); //get the type of the bullet
+			current = mats[type]; //set the variable current based on the type of the bullet
+			col.sharedMaterial = current; //set the material of the block to the current material 
+			col.enabled = false; //disable block's collider
+			col.enabled = true; //then re-enable block's collider to make changes take effect
 
 			Destroy(obj.gameObject);
 		}
 	}
-	/*
-	public void chooseMat(int i)
-	{
-		current = mats [0];
-		col.sharedMaterial = current;
-		col.enabled = false;
-		col.enabled = true;
-	}*/
 }
--- Game/Assets/Scripts/PlayerControl.cs
+++ Game/Assets/Scripts/PlayerControl.cs
@@ -4,63 +4,70 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	//public variables
-	public float speed;
-	public float maxSpeed = 5f;
-	public float jumpForce;
-	public GameObject projectile;
+	public float speed; //speed of the player
+	public float maxSpeed = 5f; //maximum speed of the player
+	public float jumpForce; //contorls how high the player will jump
+	public GameObject projectile; //what the player will shoot
 
 	//private variables
-	Rigidbody2D rb;
-	float horiz = 0f;
-	bool jump = false;
-	bool left = false;
+	Rigidbody2D rb; //rigidbody of the player
+	float horiz = 0f; //the value of the horizontal axis
+	bool jump = false; //whether the player is jumping or not 
+	bool left = false; //whether the player is facing left or not
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
+		rb = GetComponent<Rigidbody2D> (); //get reference to the rigidbody of the player
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//shooting
+		//if key F is pressed - call Fire function to fire bullet
 		if (Input.GetKeyDown (KeyCode.F)) {
 			Fire ();
 		}
 
 		//move
-		horiz = Input.GetAxis ("Horizontal");
+		horiz = Input.GetAxis ("Horizontal"); //get value of horizontal access
 
 		//press jump key
+		//if space is pressed set jump to true so you can jump
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			jump = true;
 		}
 
 		//determine if character is facing left or right
+		//if the left arrow or a is pressed then the player is going left - so left is true
 		if (Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.A)) {
 			left = true;
 		}
+
+		//if the right arrow or d is pressed then the player is going right - so left is false
 		if (Input.GetKeyDown (KeyCode.RightArrow) || Input.GetKeyDown (KeyCode.D)) {
 			left = false;
 		}
 	}
 
+	//used fixed update for changes to physics
 	void FixedUpdate()
 	{
 		//jump
 		if (jump == true) {
-			rb.AddForce(new Vector2(0f, jumpForce));
-			jump = false;
+			rb.AddForce(new Vector2(0f, jumpForce)); //if jump is true then add a force in the y direction to the player
+			jump = false; //set jump to false so that not continuously adding the upward force
 		}
 
 		//general moving
-		rb.AddForce (Vector2.right * horiz * speed);
-		if (Mathf.Abs (rb.velocity.x) > maxSpeed) 
+		rb.AddForce (Vector2.right * horiz * speed); //add a force to the player in the x direction
+		if (Mathf.Abs (rb.velocity.x) > maxSpeed)  //if the player's speed is more than the maximum speed:
 		{
-			rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+			rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y); //set the speed back to the maximum
 		}
 	}
 
 	//custom functions
+	//private function by default
 	void Fire()
 	{	
 		BulletScript.left = left;  //set the left variable for bullets so they shoot in right direction
