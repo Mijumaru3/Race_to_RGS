b9fc90bdf7213234d863292ae9d33b894456009c
Andy <awong7133@gmail.com>
2018-06-24 17:10:11 -0400

    Basic movement. Temporary player model imported from asset store

---
+++ LightsOn_3D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// VARIABLES
+	private float _speed = 10f;
+	private float _rotation_speed = 100f;
+
+	private Animator _animator;
+	private Rigidbody _rb;
+
+	// FUNCTIONS
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponentInChildren<Animator> ();
+		_rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_animator == null) {
+			return;
+		}
+
+		float in_x = Input.GetAxis ("Horizontal");
+		float in_y = Input.GetAxis ("Vertical");
+
+		Move (in_x, in_y);
+	}
+
+	private void Move(float x, float y)
+	{
+		_animator.SetFloat ("VelX", x);
+		_animator.SetFloat ("VelY", y);
+
+		/*
+		transform.position += Vector3.forward * _speed * y * Time.deltaTime;
+		transform.position += Vector3.right * _speed * x * Time.deltaTime;
+		*/
+
+		float translation = y * _speed * Time.deltaTime;
+		float rotation = x * _rotation_speed * Time.deltaTime;
+
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotation, 0);
+	}
+
+	public void OnCallChangeFace()
+	{
+	}
+}
---
+++ LightsOn_3D/Assets/Scripts/WalkController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Movement script for the player
+
+public class WalkController : MonoBehaviour {
+
+	// MOVEMENT VARIABLES
+	public float velocity_ = 6f;
+	public float max_velocity_ = 10f;
+	public float time_to_zero_max_ = 2.5f;
+	private float acceleration_;
+	private Rigidbody rb_;
+
+	// ROTATION SPEED VARIABLE(S)
+	public float rotation_speed_ = 100f;
+
+	// ==============================================================================
+	// UNITY FUNCTIONS
+
+	void Awake ()
+	{
+		rb_ = GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		acceleration_ = max_velocity_ / time_to_zero_max_;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// MOVEMENT
+		velocity_ += acceleration_ * Time.deltaTime;
+		velocity_ = Mathf.Min (velocity_, max_velocity_);
+		/*
+		float translation = Input.GetAxis("Vertical") * velocity_;
+		float rotation = Input.GetAxis("Horizontal") * rotation_speed_;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate(0, 0, translation);
+		transform.Rotate(0, rotation, 0);
+		*/
+	}
+
+	void FixedUpdate()
+	{
+		rb_.velocity = transform.forward * velocity_;
+	}
+
+	// ==============================================================================
+	// CUSTOM FUNCTIONS
+}
