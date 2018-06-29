449adb5942d5c06cfb305e8424cd8bd681470049
Andy <awong7133@gmail.com>
2018-06-29 10:16:24 -0400

    Jumping & grounding check

--- LightsOn_3D/Assets/Scripts/PlayerMovement.cs
+++ LightsOn_3D/Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,16 @@ public class PlayerMovement : MonoBehaviour {
 	// VARIABLES
 
 	// ground check variables
-	/*
-	public float _hit_distance;
-	public LayerMask _layer_mask;*/
-	private bool _is_grounded = true;
+	private bool _in_air = false;
+	private bool _jump = false;
 
 	// movement variables
 	private float _speed = 10f;
-	private float _jump_force = 10f;
+	private float _jump_force = 7f;
 	private Vector3 movement = Vector3.zero;
 
 	// component variables
-	private Animator _animator;
+	//private Animator _animator;
 	private Rigidbody _rb;
 	private Collider _collider;
 
@@ -26,23 +24,20 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		_animator = GetComponent<Animator> ();
+		//_animator = GetComponent<Animator> ();
 		_rb = GetComponent<Rigidbody> ();
 		_collider = GetComponentInChildren<Collider> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (_animator == null) {
-			Debug.Log ("No animator on gameobject");
-			return;
-		}
-
 		// movement
 		float horizontal = Input.GetAxis ("Horizontal");
 		float vertical = Input.GetAxis ("Vertical");
 
 		movement = new Vector3 (horizontal, 0.0f, vertical);
+
+		/*
 		if(movement != Vector3.zero)
 		{
 			_animator.SetBool ("Input", true);
@@ -51,82 +46,55 @@ public class PlayerMovement : MonoBehaviour {
 		{
 			_animator.SetBool ("Input", false);
 		}
-		/*
-		// jumping
-		if(_is_grounded)
-		{
-			_hit_distance = 0.35f;
-		}
-		else
-		{
-			_hit_distance = 0.15f;
-		}
+		*/
 
-		if(Physics.Raycast(transform.position - new Vector3(0, 0.85f, 0), -transform.up, _hit_distance, _layer_mask))
+		// jumping
+		if(Input.GetKeyDown(KeyCode.Space))
 		{
-			_is_grounded = true;
+			_jump = true;
 		}
-		else
-		{
-			_is_grounded = false;
-		}*/
 	}
 
 	void FixedUpdate()
 	{
-
-		_rb.velocity = movement * _speed;
-
-		if(movement != Vector3.zero)
+		if(!_in_air)
 		{
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
-		}
+			_rb.velocity = movement * _speed;
+
+			/*
+			 * rotate to match the direction of movement
+			if(movement != Vector3.zero)
+			{
+				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+			}
+			*/
 
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			Debug.Log (Physics.Raycast (transform.position, Vector3.down, 10f));
-			//Grounded ();
-			//_rb.AddForce (Vector3.up * _jump_force);
-			//_rb.AddForce (Vector3.up * _jump_force, ForceMode.Impulse);
-			_rb.AddForce (transform.up * _jump_force, ForceMode.Impulse);
-			//_rb.velocity += new Vector3(0, 30.0f, 0) + (3.0f * gameObject.transform.forward);
-			//_rb.AddForce(Vector3.up * _jump_force, ForceMode.VelocityChange);
+			if (_jump)
+			{
+				//_rb.AddForce (Vector3.up * _jump_force);
+				_rb.AddForce (Vector3.up * _jump_force, ForceMode.Impulse);
+				//_rb.AddForce (transform.up * _jump_force, ForceMode.Impulse);
+				//_rb.velocity += new Vector3(0, _jump_force, 0) + (3.0f * gameObject.transform.forward);
+				_jump = false;
+				_in_air = true;
+				//_rb.AddForce(Vector3.up * _jump_force, ForceMode.VelocityChange);
+			}			
 		}
 	}
-		
-	// CUSTOM FUNCTIONS
 
-	// check if there is a collider below the player character
-	private bool Grounded()
+	void OnCollisionEnter(Collision col)
 	{
-		/*
-		Debug.DrawRay(transform.position, Vector3.down, Color.green);
-		if(Physics.Raycast (transform.position, Vector3.down, _collider.bounds.extents.y))
+		if(col.gameObject.tag == "Platform")
 		{
-			return true;
+			Vector3 direction = gameObject.transform.position - col.gameObject.transform.position;
+			direction.Normalize ();
+			//Debug.Log (direction.y);
+
+			if(direction.y > 0)
+			{
+				_in_air = false;
+			}
 		}
-
-		return false;
-		*/
-
-		Vector3 ray_origin = _collider.bounds.center;
-		float ray_distance = _collider.bounds.extents.y + 0.1f;
-
-		Ray ray = new Ray ();
-		ray.origin = ray_origin;
-		ray.direction = Vector3.down;
-
-		if(Physics.Raycast(ray, ray_distance, 1 << 8))
-		{
-			Debug.Log ("Jump");
-			_rb.AddForce(Vector3.up * _jump_force, ForceMode.VelocityChange);
-			return true;
-		}
-
-		return false;
-	}
-
-	public void OnCallChangeFace()
-	{
 	}
 }
